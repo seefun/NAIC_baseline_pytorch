8b90331f12c0dd1ade5e0fea5044ed17471d5857
SeeFun <seefun@outlook.com>
2020-08-31 16:58:27 +0800

    Add files via upload

---
+++ Model_define_pytorch.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python3
+"""An Implement of an autoencoder with pytorch.
+This is the template code for 2020 NIAC https://naic.pcl.ac.cn/.
+The code is based on the sample code with tensorflow for 2020 NIAC and it can only run with GPUS.
+If you have any questions, please contact me with https://github.com/xufana7/AutoEncoder-with-pytorch
+Author, Fan xu Aug 2020
+
+changed by seefun Aug 2020 
+github.com/seefun | kaggle.com/seefun
+"""
+import numpy as np
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from collections import OrderedDict
+
+
+# This part implement the quantization and dequantization operations.
+# The output of the encoder must be the bitstream.
+def Num2Bit(Num, B):
+    Num_ = Num.type(torch.uint8)
+
+    def integer2bit(integer, num_bits=B * 2):
+        dtype = integer.type()
+        exponent_bits = -torch.arange(-(num_bits - 1), 1).type(dtype)
+        exponent_bits = exponent_bits.repeat(integer.shape + (1,))
+        out = integer.unsqueeze(-1) // 2 ** exponent_bits
+        return (out - (out % 1)) % 2
+
+    bit = integer2bit(Num_)
+    bit = (bit[:, :, B:]).reshape(-1, Num_.shape[1] * B)
+    return bit.type(torch.float32)
+
+
+def Bit2Num(Bit, B):
+    Bit_ = Bit.type(torch.float32)
+    Bit_ = torch.reshape(Bit_, [-1, int(Bit_.shape[1] / B), B])
+    num = torch.zeros(Bit_[:, :, 1].shape).cuda()
+    for i in range(B):
+        num = num + Bit_[:, :, i] * 2 ** (B - 1 - i)
+    return num
+
+
+class Quantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = torch.round(x * step - 0.5)
+        out = Num2Bit(out, B)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of constant arguments to forward must be None.
+        # Gradient of a number is the sum of its four bits.
+        b, _ = grad_output.shape
+        grad_num = torch.sum(grad_output.reshape(b, -1, ctx.constant), dim=2)
+        return grad_num, None
+
+
+class Dequantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = Bit2Num(x, B)
+        out = (out + 0.5) / step
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of non-Tensor arguments to forward must be None.
+        # repeat the gradient of a Num for four time.
+        #b, c = grad_output.shape
+        #grad_bit = grad_output.repeat(1, 1, ctx.constant) 
+        #return torch.reshape(grad_bit, (-1, c * ctx.constant)), None
+        grad_bit = grad_output.repeat_interleave(ctx.constant, dim=1)
+        return grad_bit, None
+
+
+class QuantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(QuantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Quantization.apply(x, self.B)
+        return out
+
+
+class DequantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(DequantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Dequantization.apply(x, self.B)
+        return out
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=True)
+
+
+class ConvBN(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, groups=1):
+        if not isinstance(kernel_size, int):
+            padding = [(i - 1) // 2 for i in kernel_size]
+        else:
+            padding = (kernel_size - 1) // 2
+        super(ConvBN, self).__init__(OrderedDict([
+            ('conv', nn.Conv2d(in_planes, out_planes, kernel_size, stride,
+                               padding=padding, groups=groups, bias=False)),
+            ('bn', nn.BatchNorm2d(out_planes))
+        ]))
+
+
+class CRBlock(nn.Module):
+    def __init__(self):
+        super(CRBlock, self).__init__()
+        self.path1 = nn.Sequential(OrderedDict([
+            ('conv3x3', ConvBN(16, 16, 3)),
+            ('relu1', nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+            ('conv1x9', ConvBN(16, 16, [1, 9])),
+            ('relu2', nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+            ('conv9x1', ConvBN(16, 16, [9, 1])),
+        ]))
+        self.path2 = nn.Sequential(OrderedDict([
+            ('conv1x5', ConvBN(16, 16, [1, 5])),
+            ('relu', nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+            ('conv5x1', ConvBN(16, 16, [5, 1])),
+        ]))
+        self.conv1x1 = ConvBN(16 * 2, 16, 1)
+        self.identity = nn.Identity()
+        self.relu = nn.LeakyReLU(negative_slope=0.3, inplace=True)
+
+    def forward(self, x):
+        identity = self.identity(x)
+
+        out1 = self.path1(x)
+        out2 = self.path2(x)
+        out = torch.cat((out1, out2), dim=1)
+        out = self.relu(out)
+        out = self.conv1x1(out)
+
+        out = self.relu(out + identity)
+        return out
+
+
+class Encoder(nn.Module):
+    B = 4
+
+    def __init__(self, feedback_bits, quantization=True):
+        super(Encoder, self).__init__()
+        self.encoder1 = nn.Sequential(OrderedDict([
+            ("conv3x3_bn", ConvBN(2, 16, 3)),
+            ("relu1", nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+            ("conv1x9_bn", ConvBN(16, 16, [1, 9])),
+            ("relu2", nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+            ("conv9x1_bn", ConvBN(16, 16, [9, 1])),
+        ]))
+        self.encoder2 = ConvBN(2, 16, 3)
+        self.encoder_conv = nn.Sequential(OrderedDict([
+            ("relu1", nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+            ("conv1x1_bn", ConvBN(16*2, 2, 1)),
+            ("relu2", nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+        ]))
+
+        self.fc = nn.Linear(1024, int(feedback_bits / self.B))
+        self.sig = nn.Sigmoid()
+        self.quantize = QuantizationLayer(self.B)
+        self.quantization = quantization 
+
+    def forward(self, x):
+        encode1 = self.encoder1(x)
+        encode2 = self.encoder2(x)
+        out = torch.cat((encode1, encode2), dim=1)
+        out = self.encoder_conv(out)
+        out = out.view(-1, 1024)
+        out = self.fc(out)
+        out = self.sig(out)
+        if self.quantization:
+            out = self.quantize(out)
+        elif self.quantization == 'check':
+            out = out
+        else:
+            out = self.fake_quantize(out)
+        return out
+
+
+class Decoder(nn.Module):
+    B = 4
+
+    def __init__(self, feedback_bits, quantization=True):
+        super(Decoder, self).__init__()
+        self.feedback_bits = feedback_bits
+        self.dequantize = DequantizationLayer(self.B)
+        self.fc = nn.Linear(int(feedback_bits / self.B), 1024)
+        decoder = OrderedDict([
+            ("conv5x5_bn", ConvBN(2, 16, 5)),
+            ("relu", nn.LeakyReLU(negative_slope=0.3, inplace=True)),
+            ("CRBlock1", CRBlock()),
+            ("CRBlock2", CRBlock())
+        ])
+        self.decoder_feature = nn.Sequential(decoder)
+        self.out_cov = conv3x3(16, 2)
+        self.sig = nn.Sigmoid()
+        self.quantization = quantization        
+
+    def forward(self, x):
+        if self.quantization:
+            out = self.dequantize(x)
+        else:
+            out = x
+        out = out.view(-1, int(self.feedback_bits / self.B))
+        out = self.fc(out)
+        out = out.view(-1, 2, 16, 32)
+        out = self.decoder_feature(out)
+        out = self.out_cov(out)
+        out = self.sig(out)
+        return out
+
+
+# Note: Do not modify following class and keep it in your submission.
+# feedback_bits is 128 by default.
+class AutoEncoder(nn.Module):
+
+    def __init__(self, feedback_bits):
+        super(AutoEncoder, self).__init__()
+        self.encoder = Encoder(feedback_bits)
+        self.decoder = Decoder(feedback_bits)
+
+    def forward(self, x):
+        feature = self.encoder(x)
+        out = self.decoder(feature)
+        return out
+
+
+def NMSE(x, x_hat):
+    x_real = np.reshape(x[:, :, :, 0], (len(x), -1))
+    x_imag = np.reshape(x[:, :, :, 1], (len(x), -1))
+    x_hat_real = np.reshape(x_hat[:, :, :, 0], (len(x_hat), -1))
+    x_hat_imag = np.reshape(x_hat[:, :, :, 1], (len(x_hat), -1))
+    x_C = x_real - 0.5 + 1j * (x_imag - 0.5)
+    x_hat_C = x_hat_real - 0.5 + 1j * (x_hat_imag - 0.5)
+    power = np.sum(abs(x_C) ** 2, axis=1)
+    mse = np.sum(abs(x_C - x_hat_C) ** 2, axis=1)
+    nmse = np.mean(mse / power)
+    return nmse
+
+def NMSE_cuda(x, x_hat):
+    x_real = x[:, 0, :, :].view(len(x),-1) - 0.5
+    x_imag = x[:, 1, :, :].view(len(x),-1) - 0.5
+    x_hat_real = x_hat[:, 0, :, :].view(len(x_hat), -1) - 0.5
+    x_hat_imag = x_hat[:, 1, :, :].view(len(x_hat), -1) - 0.5
+    power = torch.sum(x_real**2 + x_imag**2, axis=1)
+    mse = torch.sum((x_real-x_hat_real)**2 + (x_imag-x_hat_imag)**2, axis=1)
+    nmse = torch.sum(mse/power)
+    return nmse
+    
+
+def Score(NMSE):
+    score = 1 - NMSE
+    return score
+
+
+# dataLoader
+class DatasetFolder(Dataset):
+
+    def __init__(self, matData):
+        self.matdata = matData
+
+    def __len__(self):
+        return self.matdata.shape[0]
+    
+    def __getitem__(self, index):
+        return self.matdata[index] #, self.matdata[index]
---
+++ Model_train.py
@@ -0,0 +1,153 @@
+'''
+seefun . Aug 2020.
+github.com/seefun | kaggle.com/seefun
+'''
+
+import numpy as np
+import h5py
+import torch
+import os
+import torch.nn as nn
+import random
+
+from Model_define_pytorch import AutoEncoder, DatasetFolder, NMSE_cuda
+
+# Parameters for training
+gpu_list = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = gpu_list
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYHTONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    
+SEED = 42
+seed_everything(SEED) 
+
+batch_size = 256
+epochs = 100
+learning_rate = 5e-4
+num_workers = 4
+print_freq = 500  
+train_test_ratio = 0.8
+# parameters for data
+feedback_bits = 128
+img_height = 16
+img_width = 32
+img_channels = 2
+
+
+# Model construction
+model = AutoEncoder(feedback_bits)
+
+model.encoder.quantization = False
+model.decoder.quantization = False
+
+if len(gpu_list.split(',')) > 1:
+    model = torch.nn.DataParallel(model).cuda()  # model.module
+else:
+    model = model.cuda()
+
+criterion = nn.MSELoss()
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=2e-4)
+
+
+# Data loading
+data_load_address = './data'
+mat = h5py.File(data_load_address + '/H_train.mat', 'r')
+data = np.transpose(mat['H_train'])  # shape=(320000, 1024)
+data = data.astype('float32')
+data = np.reshape(data, [len(data), img_channels, img_height, img_width])
+# split data for training(70%) and validation(30%)
+np.random.shuffle(data)
+start = int(data.shape[0] * train_test_ratio)
+x_train, x_test = data[:start], data[start:]
+
+# dataLoader for training
+train_dataset = DatasetFolder(x_train)
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True, drop_last=True)
+
+# dataLoader for training
+test_dataset = DatasetFolder(x_test)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+
+
+
+best_loss = 100
+for epoch in range(epochs):
+    print('========================')
+    print('lr:%.4e'%optimizer.param_groups[0]['lr']) 
+    # model training
+    model.train()
+    if epoch < epochs//10:
+        try:
+            model.encoder.quantization = False
+            model.decoder.quantization = False
+        except:
+            model.module.encoder.quantization = False
+            model.module.decoder.quantization = False
+    else:
+        try:
+            model.encoder.quantization = True
+            model.decoder.quantization = True
+        except:
+            model.module.encoder.quantization = True
+            model.module.decoder.quantization = True
+        
+    if epoch == epochs//4 * 3:
+        optimizer.param_groups[0]['lr'] =  optimizer.param_groups[0]['lr'] * 0.25
+    
+    for i, input in enumerate(train_loader):
+            
+        input = input.cuda()
+        output = model(input)
+        
+        loss = criterion(output, input)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5.0, norm_type=2)
+        optimizer.step()
+        optimizer.zero_grad()
+        
+        if i % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Loss {loss:.4f}\t'.format(
+                epoch, i, len(train_loader), loss=loss.item()))
+    model.eval()
+    try:
+        model.encoder.quantization = True
+        model.decoder.quantization = True
+    except:
+        model.module.encoder.quantization = True
+        model.module.decoder.quantization = True
+    total_loss = 0
+    with torch.no_grad():
+        for i, input in enumerate(test_loader):
+            # convert numpy to Tensor
+            input = input.cuda()
+            output = model(input)
+            total_loss += NMSE_cuda(input, output).cpu().numpy()
+        average_loss = total_loss / len(test_dataset)
+        print('NMSE %.4f'%average_loss)
+        if average_loss < best_loss:
+            # model save
+            # save encoder
+            modelSave1 = './Modelsave/encoder.pth.tar'
+            try:
+                torch.save({'state_dict': model.encoder.state_dict(), }, modelSave1)
+            except:
+                torch.save({'state_dict': model.module.encoder.state_dict(), }, modelSave1)
+            # save decoder
+            modelSave2 = './Modelsave/decoder.pth.tar'
+            try:
+                torch.save({'state_dict': model.decoder.state_dict(), }, modelSave2)
+            except:
+                torch.save({'state_dict': model.module.decoder.state_dict(), }, modelSave2)
+            print('Model saved!')
+            best_loss = average_loss
+
